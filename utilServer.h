ab7260dc233a1c179dc1eb7a03d163af8a89520e
Bilel S <bilel.saghrouchni@gmail.com>
2021-01-06 20:17:18 +0100

    first utilServer.h

--- utilServer.h
+++ utilServer.h
@@ -3,3 +3,16 @@ void intToSeqN(int x, char* res);
 char **splitData(char* src, int fragSize);
 int readAndSendFile(int sock, struct sockaddr_in client, char* filename, int dataSize, int seqNsize, int initAck);
 int seqNToInt(char *seqNumber);
+
+struct model_elem{
+        int seqN;
+        long us;
+        struct model_elem *suivant;
+};
+
+typedef struct model_elem ELEMLIST;
+
+typedef ELEMLIST *LISTE;
+
+void insertionListeTriee(LISTE *pliste, int ackN, long us_time);
+long suppFirstOcc(LISTE *pliste, int ackN);
